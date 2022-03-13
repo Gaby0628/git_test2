89396de8941e5a1c253b53bcbf994c3dda7addd7
Gaby <1242212106@qq.com>
2022-03-13 14:15:12 +0800

    first commit

---
+++ src/main/java/com/gjs/java/GitTest.java
@@ -0,0 +1,11 @@
+package com.gjs.java;
+
+/**
+ * @author gjs
+ * @create 2022-03-13 14:13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("first git commit");
+    }
+}
